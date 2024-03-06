fc7264b035fdd6be17da39b7f0d7106ec63ed64b
Paulo Silva <pauloalmeida1288@gmail.com>
2024-03-06 12:01:14 +0000

    Ex01 testado e concluído

---
+++ RightSize/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RightSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(string s in args)
+            {
+                if (s.Length > 6)
+                    break;
+                if (s.Length > 3)
+                    Console.WriteLine(s);
+            }
+        }
+    }
+}
