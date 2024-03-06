64886d805a14b990c4c77167f39c25472368024a
Paulo Silva <pauloalmeida1288@gmail.com>
2024-03-06 12:43:36 +0000

    Ex02 primeiros testes

---
+++ ChangeString/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.Write("String: ");
+            string palavra = Console.ReadLine();
+            
+            Console.Write("\nCaráter: ");
+            string letra_s = Console.ReadLine();
+            char letra = char.Parse(letra_s);
+
+            Console.Write("\nSerá impresso: ");
+
+            foreach(char palavra_pequena in palavra){
+                if(palavra_pequena == letra)
+                {
+                    Console.Write(letra);
+                }
+                else{
+                    Console.Write(palavra_pequena);
+                }
+            }
+        }
+    }
+}
