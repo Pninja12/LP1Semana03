6acbe5ad7552a8d721c5632a9a5fcd05d57b1bba
Paulo Silva <pauloalmeida1288@gmail.com>
2024-03-06 13:00:25 +0000

    Ex02 comentado

--- ChangeString/Program.cs
+++ ChangeString/Program.cs
@@ -5,20 +5,24 @@ namespace ChangeString
     class Program
     {
         /// <summary>
-        /// 
+        /// Programa que pede uma string e um char ao utilizador e troca todas
+        /// as letra na string que coicidem com o char por um X
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
+            //pede a string ao utilizador e guarda a variável
             Console.Write("String: ");
             string palavra = Console.ReadLine();
             
+            //pede a string ao utilizador, troca para char e guarda a variável
             Console.Write("Caráter: ");
             string letra_s = Console.ReadLine();
             char letra = char.Parse(letra_s);
 
             Console.Write("Será impresso: ");
-
+            //mostra no ecrã letra a letra da palavra e caso alguma delas
+            //coicida com a letra escolhida pelo jogador, imprime um X 
             foreach(char palavra_pequena in palavra){
                 if(palavra_pequena == letra)
                 {
