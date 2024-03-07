10dfb6afd4b2e27a95965133e0f30c99a8cc43be
Paulo Silva <pauloalmeida1288@gmail.com>
2024-03-07 10:11:09 +0000

    Ex03 listas adicionadas que recebem todos os parâmetros

--- ArrayMult/Program.cs
+++ ArrayMult/Program.cs
@@ -6,10 +6,17 @@ namespace ArrayMult
     {
         static void Main(string[] args)
         {
-            int[,] A = new int[2, 2];
-            int[,] B = new int[1, 1];
+            float[,] A = new float[2, 2];
+            float[,] B = new float[2, 1];
 
-            Console.WriteLine(args[1]);
+            for(int i = 0; i < 2; i++){
+                for(int j = 0; i < 2; j++){
+                    int cont = i + j;
+                    A[i, j] = float.Parse(args[i + j]);
+                }
+            }
+            B[0,0] = float.Parse(args[5]);
+            B[1,0] = float.Parse(args[6]);
         }
     }
 }
