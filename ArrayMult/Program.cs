43c6f04ccabcc5fc7a4cb823f0eda925cf678596
Paulo Silva <pauloalmeida1288@gmail.com>
2024-03-07 10:10:44 +0000

    Ex03 Primeiros testes

---
+++ ArrayMult/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ArrayMult
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] A = new int[2, 2];
+            int[,] B = new int[1, 1];
+
+            Console.WriteLine(args[1]);
+        }
+    }
+}
