6b48f430cb4b1eb91c0f56a2f87a5d8d913de7ba
Paulo Silva <pauloalmeida1288@gmail.com>
2024-03-07 10:55:12 +0000

    Ex03 programa quase concluído, testes finais

--- ArrayMult/Program.cs
+++ ArrayMult/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace ArrayMult
 {
@@ -9,14 +10,29 @@ namespace ArrayMult
             float[,] A = new float[2, 2];
             float[,] B = new float[2, 1];
 
-            for(int i = 0; i < 2; i++){
-                for(int j = 0; i < 2; j++){
-                    int cont = i + j;
-                    A[i, j] = float.Parse(args[i + j]);
+            
+            
+
+            for(int i = 0; i < 3; i += 2){
+                for(int j = 0; j < 2; j++){
+                    A[i/2, j] = float.Parse(args[i + j]);
                 }
             }
-            B[0,0] = float.Parse(args[5]);
-            B[1,0] = float.Parse(args[6]);
+            
+            B[0,0] = float.Parse(args[4]);
+            B[1,0] = float.Parse(args[5]);
+
+            float[,] AvezesB = new float[2, 2];
+
+            for(int i = 0; i < 2; i++){
+                for(int j = 0; j < 2; j++){
+                    AvezesB[i, j] = A[i, j] * B[i, 0];
+                }
+            }
+
+            foreach(float numero in AvezesB){
+                Console.WriteLine(numero);
+            }
         }
     }
 }
