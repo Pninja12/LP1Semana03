4b1d2977dcb345f85d10e90cc18297d7dc691f78
Paulo Silva <pauloalmeida1288@gmail.com>
2024-03-07 15:17:56 +0000

    Ex4 concluído e comentado

--- HeroPerks/Program.cs
+++ HeroPerks/Program.cs
@@ -4,12 +4,20 @@ namespace HeroPerks
 {
     class Program
     {
+        /// <summary>
+        /// Programa que recebe uma palavra e que dependente do conteudo, pode 
+        /// cortar o programa, imprimir que não tem nada, ou imprimir as roles
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            //variáveis
             Perks perks = 0;
 
             bool broke = false;
 
+            //loop que vê letra a letra da string dada e atribui caso a letra 
+            //dê certo ou para o programa se der errado
             foreach(char letra in args[0]){
                 if (letra == 'w')
                     perks ^= Perks.WaterBreathing;
@@ -26,7 +34,9 @@ namespace HeroPerks
                 }
 
             }
-
+            //caso o nenuma letra seja errada, então mostra que atributos o
+            //jogador tem, caso tenha, e mostra outros extra que o jogador
+            //pode ou não ter ou se tem alguma coisa se quer
             if (!broke)
             {
                 if (perks == 0)
