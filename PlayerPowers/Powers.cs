0e792383802f991d29faae65e5c110e59d2944a4
Paulo Silva <pauloalmeida1288@gmail.com>
2024-03-08 14:58:15 +0000

    ExExtra concluído e testado

---
+++ PlayerPowers/Powers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlayerPowers
+{
+    [Flags]
+    enum Powers
+    {
+        Fly = 1 << 0,
+        XRayVision = 1 << 1,
+        SuperStrength = 1 << 2
+    }
+}
---
+++ PlayerPowers/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace PlayerPowers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos jogadores queres?\n-> ");
+            int n = int.Parse(Console.ReadLine());
+
+            Powers[] players = new Powers[n];
+
+            for(int i = 0; i < players.Length; i++)
+            {
+                Powers currentchoice = 0;
+                while(true)
+                {
+                    Console.Write($"Que poderes o {i+1}º personagem vai ter?");
+                    Console.Write("\nPoderes disponíveis: ");
+                    Console.WriteLine("Fly, XRayVision e SuperStrength");
+                    Console.Write("-> ");
+                    string resposta = Console.ReadLine();
+
+                    if(resposta == "Fly")
+                    {
+                        currentchoice ^= Powers.Fly;
+                    }
+                    if(resposta == "XRayVision")
+                    {
+                        currentchoice ^= Powers.XRayVision;
+                    }
+                    if(resposta == "SuperStrength")
+                    {
+                        currentchoice ^= Powers.SuperStrength;
+                    }
+                    Console.Write("É só isso? (Sim para acabar/");
+                    Console.Write("Não para continuar)\n->");
+                    resposta = Console.ReadLine();
+
+                    if(resposta == "Sim"){
+                        players[i] = currentchoice;
+                        break;
+                    }
+                }
+            }
+
+            for(int i = 0; i < players.Length; i++){
+                Console.Write($"O {i + 1}ª Player tem: {players[i]}");
+                if((players[i] & Powers.SuperStrength) == Powers.SuperStrength 
+                && (players[i] & Powers.Fly) == Powers.Fly)
+                {
+                    Console.Write(", Flying radiation");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+} 
