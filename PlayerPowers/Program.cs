8258960fe7f3980954b2dfbaa2f9c395c67e9553
Paulo Silva <pauloalmeida1288@gmail.com>
2024-03-08 15:03:22 +0000

    ExExtra devidamente comentado

--- PlayerPowers/Program.cs
+++ PlayerPowers/Program.cs
@@ -4,15 +4,25 @@ namespace PlayerPowers
 {
     class Program
     {
+        /// <summary>
+        /// Programa que pergunta quantos jogadores são e pede que poderes têm
+        /// para no fim dizer quais poderes cada jogador tem
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            //Quantidade de jogadores
             Console.Write("Quantos jogadores queres?\n-> ");
             int n = int.Parse(Console.ReadLine());
 
+            //Criação da lista de jogadores
             Powers[] players = new Powers[n];
 
+
+            //Loop para dar poderes a cada jogador
             for(int i = 0; i < players.Length; i++)
             {
+                //poderes a serem atribuidos no fim
                 Powers currentchoice = 0;
                 while(true)
                 {
@@ -38,6 +48,8 @@ namespace PlayerPowers
                     Console.Write("Não para continuar)\n->");
                     resposta = Console.ReadLine();
 
+                    //Se a resposta for Sim, então dá os poderes ao jogador e
+                    //vai para o próximo
                     if(resposta == "Sim"){
                         players[i] = currentchoice;
                         break;
@@ -45,8 +57,10 @@ namespace PlayerPowers
                 }
             }
 
+            //Loop para mostrar todos os jogadores e os seus poderes
             for(int i = 0; i < players.Length; i++){
                 Console.Write($"O {i + 1}ª Player tem: {players[i]}");
+                // Verifica se o jogador tem o poder secreto
                 if((players[i] & Powers.SuperStrength) == Powers.SuperStrength 
                 && (players[i] & Powers.Fly) == Powers.Fly)
                 {
