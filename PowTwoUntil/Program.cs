249b634a47ed981b4100a8b5c98167db8c665247
Paulo Silva <pauloalmeida1288@gmail.com>
2024-03-08 00:40:12 +0000

    Ex5 comentado devidamente

--- PowTwoUntil/Program.cs
+++ PowTwoUntil/Program.cs
@@ -4,13 +4,22 @@ namespace PowTwoUntil
 {
     class Program
     {
+        /// <summary>
+        /// Função que ao ser chamada escreve os multiplos de 2 até 32
+        /// </summary>
         private static void PowersOf2Until5()
         {
+            //quando um ciclo acaba, o i avança uma casa binária até chegar ao
+            //máximo de 5 casas binárias
             for (int i = 1; i <= (1<<5); i = i<<1)
             {
                 Console.WriteLine(i);
             }
         }
+        /// <summary>
+        /// O programa corre a função anterior 2 vezes
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             PowersOf2Until5();
